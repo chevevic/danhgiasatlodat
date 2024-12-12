1fd51f2a295035801211bdd1dcfbb18e56701685
chevevic <fakeminhquoc030508@gmail.com>
2024-12-12 09:30:36 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-document.cookie = "name=value; SameSite=Strict; Secure; Path=/";
 async function getweather(lat,lon) {
     try {
     const api_url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=5ecd55a45b022c71aede9450aaffb59d`;

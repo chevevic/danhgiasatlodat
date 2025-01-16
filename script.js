f4cda3a5e2cdbd6d46909f8466c48d101c72fc5c
chevevic <fakeminhquoc030508@gmail.com>
2025-01-17 00:33:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -302,3 +302,14 @@ function getDates(timestamp = Date.now()) {
         twoDaysAgo: formattedTwoDaysAgo
     };
 }
+if ('serviceWorker' in navigator) {  
+    window.addEventListener('load', () => {  
+        navigator.serviceWorker.register('/service-worker.js')  
+            .then((registration) => {  
+                console.log('Service Worker registered with scope:', registration.scope);  
+            })  
+            .catch((error) => {  
+                console.log('Service Worker registration failed:', error);  
+            });  
+    });  
+}

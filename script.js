844e8141479628e335bc3078cbb95bb5acf36998
chevevic <fakeminhquoc030508@gmail.com>
2025-01-18 01:02:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -304,7 +304,7 @@ function getDates(timestamp = Date.now()) {
 }
 if ('serviceWorker' in navigator) {  
     window.addEventListener('load', () => {  
-        navigator.serviceWorker.register('/service-worker.js')  
+        navigator.serviceWorker.register('/assets/service-worker.js')  
             .then((registration) => {  
                 console.log('Service Worker registered with scope:', registration.scope);  
             })  

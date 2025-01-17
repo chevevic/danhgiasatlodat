d77a48da2d20c0b81e4e37fd76ecdce8d3cfb101
chevevic <fakeminhquoc030508@gmail.com>
2025-01-18 01:15:14 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,49 +1,15 @@
-const CACHE_NAME = 'my-app-cache-v1';  
-
-  
-const urlsToCache = [  
-    '/',  
-    '/index.html',  
-    '/styles.css',  
-    '/script.js',  
-    '/favicon.ico', // 
 self.addEventListener('install', (event) => {  
-    event.waitUntil(  
-        caches.open(CACHE_NAME)  
-            .then((cache) => {  
-                console.log('Caching app shell');  
-                return cache.addAll(urlsToCache);  
-            })  
-    );  
+    console.log('Service Worker: Installed');  
 });  
-
 
 self.addEventListener('activate', (event) => {  
-    const cacheWhitelist = [CACHE_NAME]; 
-    event.waitUntil(  
-        caches.keys().then((cacheNames) => {  
-            return Promise.all(  
-                cacheNames.map((cacheName) => {  
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {  
-                        console.log('Deleting old cache:', cacheName);  
-                        return caches.delete(cacheName);  
-                    }  
-                })  
-            );  
-        })  
-    );  
+    console.log('Service Worker: Activated');  
 });  
-
 
 self.addEventListener('fetch', (event) => {  
     event.respondWith(  
-        caches.match(event.request)  
-            .then((response) => {  
-                 
-                if (response) {  
-                    return response;   
-                }  
-                return fetch(event.request); 
-            })  
+        caches.match(event.request).then((response) => {  
+            return response || fetch(event.request);  
+        })  
     );  
 });
